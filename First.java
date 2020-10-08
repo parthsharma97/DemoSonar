5430a551f69af8e911c785a01be4be88ef57b3f1
parthsharma97 <55278237+parthsharma97@users.noreply.github.com>
2020-10-08 17:26:33 +0530

    Update First.java

--- First.java
+++ First.java
@@ -3,10 +3,10 @@ public class First {
 	public static void main(String args[]){
 		
 		System.out.println("Hello!!");
-    System.out.println("Hello!!!");
-    System.out.println("Hello!!!!");
+    		System.out.println("Hello!!!");
+    		System.out.println("Hello!!!!");
 		System.out.println("Hello!!!!!");
-    System.out.println("Hello!!!!!");
+   		System.out.println("Hello!!!!!");
 	}
 
 }
